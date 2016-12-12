a7c279050087626eb57a6c46d847af24e74c34b5
DESKTOP-00JB3JF\User <ernestwkwso@gmail.com>
2016-12-12 10:03:45 +0800

    .

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+            <script type="text/javascript">location.href = 'index.html';</script>
+
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
